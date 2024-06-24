e0466e78950e6122e28419899bc3bb2e08925582
siratchauhan <138846300+siratchauhan@users.noreply.github.com>
2024-06-24 10:28:31 +0530

    Add files via upload

---
+++ Weather-App/weatherapp/views.py
@@ -0,0 +1,57 @@
+from django.shortcuts import render
+from django.contrib import messages
+import requests
+import datetime
+
+
+def home(request):
+   
+    if 'city' in request.POST:
+         city = request.POST['city']
+    else:
+         city = 'chandigarh'     
+    
+    url = f'https://api.openweathermap.org/data/2.5/weather?q={city}&appid=975e35c0ae3598a98b5e8c3db894b18f'
+    PARAMS = {'units':'metric'}
+
+    API_KEY =  'AIzaSyB3plTRbe_6et1eoutNcXOMM4aElqtbsVU'
+
+    SEARCH_ENGINE_ID = 'e6c4256ec2f7045ac'
+     
+    query = city + " 1920x1080"
+    page = 1
+    start = (page - 1) * 10 + 1
+    searchType = 'image'
+    city_url = f"https://www.googleapis.com/customsearch/v1?key={API_KEY}&cx={SEARCH_ENGINE_ID}&q={query}&start={start}&searchType={searchType}&imgSize=xlarge"
+
+    data = requests.get(city_url).json()
+    count = 1
+    search_items = data.get("items")
+    image_url = search_items[1]['link']
+    
+
+    try:
+          
+          data = requests.get(url,params=PARAMS).json()
+          description = data['weather'][0]['description']
+          icon = data['weather'][0]['icon']
+          temp = data['main']['temp']
+          day = datetime.date.today()
+
+          return render(request,'weatherapp/index.html' , {'description':description , 'icon':icon ,'temp':temp , 'day':day , 'city':city , 'exception_occurred':False ,'image_url':image_url})
+    
+    except KeyError:
+          exception_occurred = True
+          messages.error(request,'Entered data is not available to API')   
+          # city = 'indore'
+          # data = requests.get(url,params=PARAMS).json()
+          
+          # description = data['weather'][0]['description']
+          # icon = data['weather'][0]['icon']
+          # temp = data['main']['temp']
+          day = datetime.date.today()
+
+          return render(request,'weatherapp/index.html' ,{'description':'clear sky', 'icon':'01d'  ,'temp':25 , 'day':day , 'city':'indore' , 'exception_occurred':exception_occurred } )
+               
+    
+    
